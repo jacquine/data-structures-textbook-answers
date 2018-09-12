cb7a31cfe7b0d4e393eadf9420e4b31f6eb06059
Jacquine <teozhenqi@gmail.com>
2018-09-12 18:10:55 -0400

    Add files via upload

---
+++ isMultiple.java
@@ -0,0 +1,14 @@
+package test123;
+
+public class isMultiple {
+	public static boolean isMultiple(long n, long m) {
+		if (n % m == 0) 
+			return true;
+		else
+			return false;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isMultiple(128, 3));
+	}
+}
